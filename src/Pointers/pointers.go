8ab3300b0d60c91f45abbdcc25233af39dc53c27
dominicjmccoy <51916628+dominicjmccoy@users.noreply.github.com>
2023-03-19 09:55:37 +0000

    pointers and struct added

--- src/Pointers/pointers.go
+++ src/Pointers/pointers.go
@@ -4,8 +4,13 @@ import "fmt"
 
 func main() {
 	i, j := 42, 2701
+	fmt.Println("i: ", i, "j: ", j)
 	p := &i // points to i
-	fmt.Println(*p)
-	q := &j
-	fmt.Println(*q)
+	fmt.Println("p*: ", *p)
+	*p = 21
+	fmt.Println("i is now: ", i)
+
+	p = &j
+	*p = *p / 37
+	fmt.Println("j is now: ", j)
 }
---
+++ src/struct/struct.go
@@ -0,0 +1,14 @@
+package main
+
+// a struct is a collection of fields
+
+import "fmt"
+
+type Vertex struct {
+	X int
+	Y int
+}
+
+func main() {
+	fmt.Println(Vertex{1, 2})
+}
